0822bf2ebce8a47c86504a8a0b5dfb42d728a01f
jacobchung <chung.jac@northeastern.edu>
2021-10-11 15:03:27 -0400

    s

--- js/main.js
+++ js/main.js
@@ -1,22 +1,90 @@
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// write your javascript code here.
-// feel free to change the pre-set attributes as you see fit
+// append the svg object to the body of the page
+const svg = d3.select("#d3-container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-let margin = {
-    top: 60,
-    left: 50,
-    right: 30,
-    bottom: 35
-  },
-  width = 500 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/ic-04-corrected-ic04-hp-jc/main/data/data.csv").then( function(data) {
 
-//SVG that will hold the visualization 
-let svg1 = d3.select('#d3-container')
-  .append('svg')
-  .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
-  .attr('width', '60%') // this is now required by Chrome to ensure the SVG shows up at all
-  .style('background-color', 'white') 
-  .style('border', 'solid')
-  .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+// X axis
+const x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(d => d.X))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .style("text-anchor", "end");
+
+// Add Y axis
+const y = d3.scaleLinear()
+  .domain([0, 100])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y));
+
+// Bars
+svg.selectAll("mybar")
+  .data(data)
+  .join("rect")
+    .attr("x", d => x(d.X))
+    .attr("y", d => y(d.Y))
+    .attr("width", x.bandwidth())
+    .attr("height", d => height - y(d.Y))
+    .attr("fill", "#FFA500")
+    
+
+    
+    //.on("mouseover", mouseover)
+    //.on("mousemove", mousemove)
+    //.on("mouseleave", mouseleave);
+
+})
+
+
+
+
+/*
+  // create a tooltip
+  var Tooltip = d3.select("#d3-container")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html("The exact value of<br>this cell is: " + d.Y)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+  } */
+
 
